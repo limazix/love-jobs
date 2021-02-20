abcbf27adae3b3e4c760344c9fe0113c7581a576
Bruno Lima Cardoso <blima@br.ibm.com>
2021-02-20 13:08:37 -0300

    Fix format with prettier

--- App.js
+++ App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 import FBLoginBtn from './components/fb-login-btn/FBLoginBtn';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-        <FBLoginBtn />
-      </SafeAreaView>
-    </>
-  );
+const App = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView style={styles.container}>
+                <FBLoginBtn />
+            </SafeAreaView>
+        </>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
 });
 
 export default App;
